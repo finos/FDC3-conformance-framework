c540e1e17000b7f1b0d7a147e7c8075284a8d5f0
Joe Dunleavy <jdunleavy@scottlogic.com>
2022-09-01 14:04:27 +0100

    merge changes to main

--- tests/src/test/fdc3.addContextListener.ts
+++ tests/src/test/fdc3.addContextListener.ts
@@ -1,4 +1,5 @@
 import { Listener } from "@finos/fdc3";
+import { assert, expect } from "chai";
 
 export default () =>
   describe("fdc3.addContextListener", () => {
@@ -13,13 +14,29 @@ export default () =>
 
     it("Method is callable", async () => {
       const contextType = "fdc3.contact";
-      listener = await window.fdc3.addContextListener(
-        contextType,
-        (info: any) => {
-          console.log(
-            `Context listener of type ${contextType} triggered with result ${info}`
-          );
-        }
-      );
+      try {
+        listener = await window.fdc3.addContextListener(
+          contextType,
+          (info: any) => {
+            console.log(
+              `Context listener of type ${contextType} triggered with result ${info}`
+            );
+          }
+        );
+      } catch (ex) {
+        assert.fail("Error while calling method. " + (ex.message ?? ex));
+      }
+    });
+
+    it("Returns listener object", async () => {
+      const contextType = "fdc3.contact";
+
+      try {
+        listener = await window.fdc3.addContextListener(null, () => {});
+        assert.isObject(listener);
+        expect(typeof listener.unsubscribe).to.be.equals("function");
+      } catch (ex) {
+        assert.fail("No listener object returned. " + (ex.message ?? ex));
+      }
     });
   });
--- tests/src/test/fdc3.getCurrentChannel.ts
+++ tests/src/test/fdc3.getCurrentChannel.ts
@@ -1,6 +1,13 @@
+import { expect } from "chai";
+
 export default () =>
   describe("fdc3.getCurrentChannel", () => {
     it("Method is callable", async () => {
       await window.fdc3.getCurrentChannel();
+    });
+
+    it("getCurrentChannel() returns null if no channel has been joined", async () => {
+      const channel = await window.fdc3.getCurrentChannel();
+      expect(channel).to.be.null;
     });
   });
