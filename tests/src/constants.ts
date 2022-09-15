ec53f94786b58ed5a01b9c1baea89b44af1a20d5
Joe Dunleavy <jdunleavy@scottlogic.com>
2022-09-15 10:37:27 +0100

    refactor code

--- tests/src/constants.ts
+++ tests/src/constants.ts
@@ -3,7 +3,7 @@
  */
 const constants = {
   Fdc3Timeout: 500, // The amount of time to wait for the FDC3Ready event during initialisation
-  TestTimeout: 7000, // Tests that take longer than this (in milliseconds) will fail
+  TestTimeout: 8000, // Tests that take longer than this (in milliseconds) will fail
   WaitTime: 3000, // The amount of time to wait for mock apps to finish processing
 } as const;
 
