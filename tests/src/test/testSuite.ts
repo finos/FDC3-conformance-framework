aae0033f0b06657857e97b92747229a7169af33b
Joe Dunleavy <jdunleavy@scottlogic.com>
2022-11-04 12:51:39 +0000

    clean up test suite file

--- tests/src/test/testSuite.ts
+++ tests/src/test/testSuite.ts
@@ -1,6 +1,5 @@
 import mocha from "mocha";
 import constants from "../constants";
-
 import fdc3AddContextListener_1_2 from "./v1.2/fdc3.addContextListener";
 import fdc3AddIntentListener_1_2 from "./v1.2/fdc3.addIntentListener";
 import fdc3Broadcast_1_2 from "./v1.2/fdc3.broadcast";
@@ -15,7 +14,6 @@ import fdc3LeaveCurrentChannel_1_2 from "./v1.2/fdc3.leaveCurrentChannel";
 import fdc3Open_1_2 from "./v1.2/fdc3.open";
 import fdc3RaiseIntent_1_2 from "./v1.2/fdc3.raiseIntent";
 import fdc3RaiseIntentForContext_1_2 from "./v1.2/fdc3.raiseIntentForContext";
-
 //import fdc3AddContextListener_2_0 from "./v2.0/fdc3.addContextListener";
 // import fdc3AddIntentListener_2_0 from "./v2.0/fdc3.addIntentListener";
 import fdc3Broadcast_2_0 from "./v2.0/fdc3.broadcast";
@@ -71,8 +69,6 @@ const testSuites_2_0 = [
  */
 export const executeTestsInBrowser = async (fdc3Version: string) => {
   (mocha as any).timeout(constants.TestTimeout);
-  //const underscoredVersionNumber = getUnderscoredVersionNumber(fdc3Version);
-  //const testSuite = getTestSuites(underscoredVersionNumber);
   if (fdc3Version === "v1.2") {
     testSuites_1_2.forEach((suite) => suite());
   } else if (fdc3Version === "v2.0") {
@@ -80,13 +76,3 @@ export const executeTestsInBrowser = async (fdc3Version: string) => {
   }
   mocha.run();
 };
-
-// function getUnderscoredVersionNumber(fdc3Version) {
-//   if (fdc3Version === "v1.2") {
-//     return "_1_0";
-//   } else if (fdc3Version === "v2.0") {
-//     return "_2_0";
-//   } else {
-//     throw new Error(`Unrecognised FDC3 API version number: ${fdc3Version}`);
-//   }
-// }
