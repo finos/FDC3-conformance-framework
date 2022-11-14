bc383a48931ffa5a97d3600439dbb81b2e7a1d19
Joe Dunleavy <jdunleavy@scottlogic.com>
2022-11-14 10:35:05 +0000

    revert changes to cmock-functions file

--- mock/mock-functions.js
+++ mock/mock-functions.js
@@ -1,20 +1,19 @@
 const onFdc3Ready = () => new Promise((resolve) => {
-    if (window.fdc3) {
-        resolve();
-    } else {
-        window.addEventListener('fdc3Ready', () => resolve());
-    }
+  if (window.fdc3) {
+      resolve();
+  } else {
+      window.addEventListener('fdc3Ready', () => resolve());
+  }
 });
 
 const closeWindowOnCompletion = async (fdc3) => {
-    const appControlChannel = await window.fdc3.getOrCreateChannel(
-        "app-control"
-      );
-    appControlChannel.addContextListener("closeWindow", async (context) => {
-        //notify app A that window was closed
-        await appControlChannel.broadcast({type: "windowClosed", testId: context.testId});
-        setTimeout(()=>{ //yield to make sure the broadcast gets out before we close
-            window.close();
-          },1);
-    });
+  const appControlChannel = await window.fdc3.getOrCreateChannel(
+      "app-control"
+    );
+  await appControlChannel.addContextListener("closeWindow", async (context) => {
+      window.close();
+
+      //notify app A that window was closed
+      await appControlChannel.broadcast({type: "windowClosed"});
+  });
 };
