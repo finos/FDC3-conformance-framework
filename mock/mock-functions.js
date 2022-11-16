c249a41a8d7e8ff681d86b0364a7b1ec58ff58a0
Joe Dunleavy <jdunleavy@scottlogic.com>
2022-11-16 18:04:59 +0000

    revert changes

--- mock/mock-functions.js
+++ mock/mock-functions.js
@@ -7,48 +7,18 @@ const onFdc3Ready = () =>
     }
   });
 
-const closeWindowOnCompletion = async (fdc3) => {
-  let implementationMetadata = await fdc3.getInfo();
-  let { fdc3Version } = implementationMetadata;
-  const appControlChannel = await fdc3.getOrCreateChannel("app-control");
-  if (fdc3Version === "1.2") {
-    await closeWindowOnCompletion_1_2(appControlChannel);
-  } else if (fdc3Version === "2.0") {
-    await closeWindowOnCompletion_2_0(appControlChannel);
-  }
-};
-
-const closeWindowOnCompletion_1_2 = async (appControlChannel) => {
-  appControlChannel.addContextListener("closeWindow", async (context) => {
-    //notify app A that window was closed
-    appControlChannel.broadcast({
-      type: "windowClosed",
-      testId: context.testId,
-    });
-    await sleep(10);
-    //yield to make sure the broadcast gets out before we close
-    window.close();
-  });
-};
-
-const closeWindowOnCompletion_2_0 = async (appControlChannel) => {
+const closeWindowOnCompletion = async () => {
+  const appControlChannel = await window.fdc3.getOrCreateChannel("app-control");
   await appControlChannel.addContextListener("closeWindow", async (context) => {
     //notify app A that window was closed
     await appControlChannel.broadcast({
       type: "windowClosed",
       testId: context.testId,
     });
-    await sleep(10);
-    //yield to make sure the broadcast gets out before we close
-    window.close();
+    setTimeout(() => {
+      //yield to make sure the broadcast gets out before we close
+      window.close();
+      return;
+    }, 5);
   });
 };
-
-async function sleep(timeoutMs) {
-  let timeout;
-  return new Promise((resolve) => {
-    timeout = window.setTimeout(() => {
-      resolve();
-    }, timeoutMs);
-  });
-}
