acb842938c27f88cf6e5cd383f48b43781821771
Rob Moffat <rob@kite9.com>
2022-11-20 17:03:28 +0000

    Fixed package.json for static

--- static/options/glue-core-replace.js
+++ static/options/glue-core-replace.js
@@ -1,5 +1,7 @@
 module.exports = {
     files: '../dist/**/index.html',
     from: '<!-- optional 1.2 scripts -->',
-    to: "hello baby"
+    to: '<script src="lib/platform.web.umd.js"></script>\n'+
+        '  <script src="lib/fdc3.umd.js"></script>\n'+
+        '  <script src="lib/startRunner.js"></script>\n'
 };
