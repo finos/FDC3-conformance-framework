8b24e7e0da8a26234745a2a17c8ce68ac9647575
Rob Moffat <rob@kite9.com>
2022-11-20 19:11:57 +0000

    Just upload the dist dir

--- static/v2.0/lib/glue/startRunner.js
+++ static/v2.0/lib/glue/startRunner.js
@@ -120,98 +120,12 @@
 		const applications = {
 			local: [
 				{
-					"name": "MockApp",
-					"title": "Mock App",
-					"description": "Testing spec conformance",
-					"type": "window",
-					"details": {
-						"url": "http://localhost:3001",
-						"top": 100,
-						"left": 100,
-						"width": 800,
-						"height": 600,
-						"icon": "http://localhost:3000/scott-logic-icon-256.png"
-					}
-				},
-				{
-					"name": "IntentAppAId",
-					"title": "Intent App A",
-					"type": "window",
-					"details": {
-						"url": "http://localhost:3100",
-						"top": 100,
-						"left": 100,
-						"width": 800,
-						"height": 600,
-						"icon": "http://localhost:3000/scott-logic-icon-256.png"
-					},
-					"intents": [
-						{
-							"name": "aTestingIntent",
-							"displayName": "A Testing Intent",
-							"contexts": ["testContextX", "testContextZ"]
-						},
-						{
-							"name": "sharedTestingIntent1",
-							"displayName": "Shared Testing Intent",
-							"contexts": ["testContextX"]
-						}
-					]
-				},
-				{
-					"name": "IntentAppBId",
-					"title": "Intent App B",
-					"description": "Testing find intent B",
-					"type": "window",
-					"details": {
-						"url": "http://localhost:3101",
-						"top": 100,
-						"left": 100,
-						"width": 800,
-						"height": 600,
-						"icon": "http://localhost:3101/scott-logic-icon-256.png"
-					},
-					"intents": [
-						{
-							"name": "bTestingIntent",
-							"displayName": "B Testing Intent",
-							"contexts": ["testContextY"]
-						},
-						{
-							"name": "sharedTestingIntent1",
-							"displayName": "Shared Testing Intent",
-							"contexts": ["testContextX", "testContextY"]
-						}
-					]
-				},
-				{
-					"name": "IntentAppCId",
-					"title": "Intent App C",
-					"description": "Testing find intent C",
-					"type": "window",
-					"details": {
-						"url": "http://localhost:3102",
-						"top": 100,
-						"left": 100,
-						"width": 800,
-						"height": 600,
-						"icon": "http://localhost:3102/scott-logic-icon-256.png"
-					},
-					"intents": [
-						{
-							"name": "cTestingIntent",
-							"displayName": "C Testing Intent",
-							"contexts": ["testContextX"]
-						}
-					]
-				},
-				{
 					"name": "ChannelsAppId",
 					"title": "Channels App",
 					"description": "Testing channels app",
 					"type": "window",
 					"details": {
-						"url": "http://localhost:3103",
+						"url": "http://localhost:3001/v2.0/channels",
 						"top": 100,
 						"left": 100,
 						"width": 800,
