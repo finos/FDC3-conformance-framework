7b092f76799e0825adf7e825762531419f17eaeb
Rob Moffat <rob@kite9.com>
2022-11-25 09:56:02 +0000

    tidying imports

--- src/mock/v1.2/intent-c.ts
+++ src/mock/v1.2/intent-c.ts
@@ -1,6 +1,5 @@
 import { closeWindowOnCompletion, onFdc3Ready } from "./mock-functions";
 import { DesktopAgent } from "fdc3_1_2/dist/api/DesktopAgent";
-import { Context } from "fdc3_1_2";
 import { sendContextToTests } from "../v1.2/mock-functions";
 import { ContextToSend } from "./general";
 declare let fdc3: DesktopAgent;
