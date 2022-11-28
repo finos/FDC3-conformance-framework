6fc5458361c2c3d5157615d25cebc230a3601566
Rob Moffat <rob@kite9.com>
2022-11-28 10:41:27 +0000

    Removed redundant asyncs

--- src/mock/v1.2/channelService-1_2.ts
+++ src/mock/v1.2/channelService-1_2.ts
@@ -1,4 +1,4 @@
-import { DesktopAgent } from "fdc3_1_2";
+import { Channel, DesktopAgent } from "fdc3_1_2";
 import { AppControlContext } from "../../test/common/channel-control";
 import { commands, channelType } from "../constants";
 declare let fdc3: DesktopAgent
@@ -65,7 +65,7 @@ export class Fdc3CommandExecutor1_2 {
   //get broadcast service and broadcast the given context type
   async broadcastContextItem(contextType, channel, historyItems, testId) {
     let broadcastService = this.getBroadcastService(channel.type);
-    await broadcastService.broadcast(contextType, historyItems, channel, testId);
+    broadcastService.broadcast(contextType, historyItems, channel, testId);
   }
 
   //get app/system channel broadcast service
@@ -79,7 +79,7 @@ export class Fdc3CommandExecutor1_2 {
 
   //app channel broadcast service
   appChannelBroadcastService = {
-    broadcast: async(contextType, historyItems, channel, testId) => {
+    broadcast: (contextType, historyItems, channel: Channel, testId) => {
       if (channel !== undefined) {
         for (let i = 0; i < historyItems; i++) {
           let context : AppControlContext = {
@@ -87,7 +87,7 @@ export class Fdc3CommandExecutor1_2 {
             name: `History-item-${i + 1}`,
           };
           if (testId) context.testId = testId;
-          await channel.broadcast(context);
+          channel.broadcast(context);
         }
       }
     },
@@ -95,14 +95,14 @@ export class Fdc3CommandExecutor1_2 {
 
   //system channel broadcast service
   systemChannelBroadcastService = {
-    broadcast: async (contextType, historyItems, ignored, testId) => {
+    broadcast: (contextType, historyItems, ignored, testId) => {
       for (let i = 0; i < historyItems; i++) {
         let context : AppControlContext = {
           type: contextType,
           name: `History-item-${i + 1}`,
         };
         if (testId) context.testId = testId;
-        await fdc3.broadcast(context);
+        fdc3.broadcast(context);
       }
     },
   };
@@ -115,7 +115,7 @@ export class Fdc3CommandExecutor1_2 {
     );
     appControlChannel.addContextListener("closeWindow", async () => {
       console.log(Date.now() + ` Received closeWindow message`);
-      await appControlChannel.broadcast({ type: "windowClosed", testId: testId } as AppControlContext);
+      appControlChannel.broadcast({ type: "windowClosed", testId: testId } as AppControlContext);
       setTimeout(() => {
         //yield to make sure the broadcast gets out before we close
         window.close();
