202b0443d7799d874369cb047ee8474d1447c27e
Rob Moffat <rob@kite9.com>
2022-12-05 10:04:31 +0000

    Dirty fix for raise intent issue

--- src/test/v1.2/advanced/fdc3.raiseIntent-good.ts
+++ src/test/v1.2/advanced/fdc3.raiseIntent-good.ts
@@ -20,7 +20,7 @@ const raiseIntentDocs =
  * Details on the mock apps used in these tests can be found in /mock/README.md
  */
 export default () =>
-  describe("fdc3.raiseIntent", () => {
+  describe("fdc3.raiseIntent (good)", () => {
     afterEach(async function afterEach() {
       await closeIntentAppsWindows(this.currentTest.title);
     });
@@ -82,105 +82,6 @@ export default () =>
       );
       validateIntentResolution("IntentAppA", intentResolution);
       await result;
-    });
-
-    const test5 =
-      "(FailedResolve1) Should fail to raise intent when targeted app intent-a, context 'testContextY' and intent 'aTestingIntent' do not correlate";
-    it(test5, async () => {
-      try {
-        await fdc3.raiseIntent(
-          "aTestingIntent",
-          {
-            type: "testContextY",
-          },
-          "IntentAppA"
-        );
-        assert.fail("Error was not thrown");
-      } catch (ex) {
-        expect(ex).to.have.property("message", ResolveError.NoAppsFound);
-
-        //raise intent so that afterEach resolves
-        await fdc3.raiseIntent("sharedTestingIntent1", {
-          type: "testContextY",
-        });
-      }
-    });
-    const test6 =
-      "(FailedResolve2) Should fail to raise intent when targeted app intent-a (name and appId), context 'testContextY' and intent 'aTestingIntent' do not correlate";
-    it(test6, async () => {
-      try {
-        await fdc3.raiseIntent(
-          "aTestingIntent",
-          {
-            type: "testContextY",
-          },
-          { name: "IntentAppA", appId: "IntentAppAId" }
-        );
-        assert.fail("Error was not thrown", raiseIntentDocs);
-      } catch (ex) {
-        expect(ex).to.have.property(
-          "message",
-          ResolveError.NoAppsFound,
-          raiseIntentDocs
-        );
-
-        //raise intent so that afterEach resolves
-        await fdc3.raiseIntent("sharedTestingIntent1", {
-          type: "testContextY",
-        });
-      }
-    });
-
-    const test7 =
-      "(FailedResolve3) Should fail to raise intent when targeted app intent-a (name), context 'testContextY' and intent 'aTestingIntent' do not correlate";
-    it(test7, async () => {
-      try {
-        await fdc3.raiseIntent(
-          "aTestingIntent",
-          {
-            type: "testContextY",
-          },
-          { name: "IntentAppA" }
-        );
-        assert.fail("Error was not thrown", raiseIntentDocs);
-      } catch (ex) {
-        expect(ex).to.have.property(
-          "message",
-          ResolveError.NoAppsFound,
-          raiseIntentDocs
-        );
-
-        //raise intent so that afterEach resolves
-        await fdc3.raiseIntent("sharedTestingIntent1", {
-          type: "testContextY",
-        });
-      }
-    });
-
-    const test8 =
-      "(FailedResolve4) Should fail to raise intent when targeted app intent-c, context 'testContextX' and intent 'aTestingIntent' do not correlate";
-    it(test8, async () => {
-      try {
-        await fdc3.raiseIntent(
-          "aTestingIntent",
-          {
-            type: "testContextX",
-          },
-          "IntentAppC"
-        );
-        assert.fail("Error was not thrown", raiseIntentDocs);
-      } catch (ex) {
-        expect(ex).to.have.property(
-          "message",
-          ResolveError.NoAppsFound,
-          raiseIntentDocs
-        );
-
-        //raise intent so that afterEach resolves
-        await fdc3.raiseIntent("sharedTestingIntent1", {
-          type: "testContextY",
-        });
-      }
     });
   });
 
