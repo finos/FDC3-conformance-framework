cb9b7e05f588718b77b116720e5d0e2488c5362a
Joe Dunleavy <jdunleavy@scottlogic.com>
2023-01-03 16:49:06 +0000

    fix RaiseIntentContextResult0secs

--- src/mock/v2.0/intent-b.ts
+++ src/mock/v2.0/intent-b.ts
@@ -4,11 +4,10 @@ import { sendContextToTests } from "../v2.0/mock-functions";
 import { wait, wrapPromise } from "../../utils";
 import { IntentUtilityContext } from "../../test/common/common-types";
 declare let fdc3: DesktopAgent;
-
 onFdc3Ready().then(async () => {
   await closeWindowOnCompletion();
 
-  //used in 'RaiseIntentTargetedAppResolve'
+  //used in 'RaiseIntentContextResult5secs'
   await fdc3.addIntentListener("sharedTestingIntent1", async (context: IntentUtilityContext) => {
     if (context.delayBeforeReturn > 0) {
       await wait(context.delayBeforeReturn);
@@ -18,10 +17,6 @@ onFdc3Ready().then(async () => {
       type: "sharedTestingIntent1-listener-triggered",
     });
 
-    if (context.type === "testContextY") {
-      return;
-    } else if (context.type === "testContextX") {
-      return context;
-    }
+    return context;
   });
 });
