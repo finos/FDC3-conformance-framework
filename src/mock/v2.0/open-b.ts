741711a176003a602e9f5793af984b9bca8fab14
Joe Dunleavy <jdunleavy@scottlogic.com>
2023-01-19 11:42:08 +0000

    remove changes to open-b

--- src/mock/v2.0/open-b.ts
+++
@@ -1,9 +0,0 @@
-import { onFdc3Ready } from "./mock-functions";
-import { wait } from "../../utils";
-import constants from "../../constants";
-
-// used in AOpensBNoListen
-onFdc3Ready().then(async () => {
-  await wait(constants.NoListenerTimeout); // make sure no listener is added until timeout is exceeded
-  window.close();
-});
