b6beaa34451afc08cbdbddcb1f69879a54023524
Gagan Singh <ahluwaliagagan@gmail.com>
2023-03-02 09:19:14 +0000

    Changes to GlueCore to make it run 1.2 tests.

--- options/glue-core-app-1.2.js
+++ options/glue-core-app-1.2.js
@@ -1,7 +1,6 @@
 module.exports = {
     files: './dist/**/index.html',
-    from: '<!-- optional 2.0 app scripts -->',
-    to: '<script src="https://unpkg.com/@glue42/web-platform@latest/dist/platform.web.umd.js"></script>\n'+
-        '  <script src="https://unpkg.com/@glue42/fdc3@3.0.0/dist/fdc3.umd.js"></script>\n'+
-        '  <script src="../lib/glue/startRunner.js"></script>\n'
+    from: '<!-- optional 1.2 app scripts -->',
+    to: '  <script src="../glue42/startRunner.js"></script>\n'+
+        '  <script src="../glue42/fdc3-glue42.js"></script>\n'
 };
--- options/glue-core-support-1.2.js
+++ options/glue-core-support-1.2.js
@@ -1,7 +1,5 @@
 module.exports = {
     files: './dist/**/index.html',
-    from: '<!-- optional 2.0 support scripts -->',
-    to: '<script src="https://unpkg.com/@glue42/web-platform@latest/dist/platform.web.umd.js"></script>\n'+
-        '  <script src="https://unpkg.com/@glue42/fdc3@3.0.0/dist/fdc3.umd.js"></script>\n'+
-        '  <script src="../lib/glue/startSupport.js"></script>\n'
+    from: '<!-- optional 1.2 support scripts -->',
+    to: '  <script src="../glue42/fdc3-glue42.js"></script>\n'
 };
