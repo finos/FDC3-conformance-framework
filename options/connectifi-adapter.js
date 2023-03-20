df221cdb7934aba90af8d8a8eebb438eb2de72a8
Nicholas Kolba <nkolba@gmail.com>
2023-03-19 22:03:23 -0400

    bumped agent version

--- options/connectifi-adapter.js
+++ options/connectifi-adapter.js
@@ -2,7 +2,7 @@ module.exports = {
     files: './dist/**/index.html',
     from: ['<!-- optional 1.2 app scripts -->', '<!-- optional 1.2 support scripts -->'],
     to: ` <script type="module" >
-             import { createAgent } from "https://unpkg.com/@connectifi/agent-web@1.0.1/dist/main.js";
+             import { createAgent } from "https://unpkg.com/@connectifi/agent-web@1.1.0/dist/main.js";
              const connect = async () => {
                   const api = await createAgent(
                      'https://dev.connectifi-interop.com',
