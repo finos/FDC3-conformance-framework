988bbf2f47a6f0aea96f7ade62049a19d7665101
Rob Moffat <rob@kite9.com>
2023-06-06 14:52:15 +0100

    Adding back close mock app

--- src/test/v2.0/basic/fdc3.basic.ts
+++ src/test/v2.0/basic/fdc3.basic.ts
@@ -28,5 +28,15 @@ export let fdc3BasicGI1_2_0 = () => describe("fdc3.basicGI1_2.0", () => basicGI1
 export let fdc3BasicAC1_2_0 = () => describe("fdc3.basicAC1_2.0", () => basicAC1(fdc3, documentation_AC));
 export let fdc3BasicUC1_2_0 = () => describe("fdc3.basicUC1_2.0", () => basicUC1(control,documentation_UC));
 export let fdc3BasicJC1_2_0  = () => describe("fdc3.basicJC1_2.0", () => basicJC1(cc, fdc3, documentation_JC));
-export let fdc3BasicRI1_2_0  = () => describe("fdc3.basicRI1_2.0", () => basicRI1(fdc3, documentation_RI, Intent.aTestingIntent, ContextType.testContextX));
-export let fdc3BasicRI2_2_0  = () => describe("fdc3.basicRI2_2.0", () => basicRI2(fdc3, documentation_RI, ContextType.testContextZ));
+export let fdc3BasicRI1_2_0  = () => describe("fdc3.basicRI1_2.0", () => {
+    after(async function after() {
+        await closeMockAppWindow(this.currentTest.title);
+    });
+    basicRI1(fdc3, documentation_RI, Intent.aTestingIntent, ContextType.testContextX)
+});
+export let fdc3BasicRI2_2_0  = () => describe("fdc3.basicRI2_2.0", () => { 
+    after(async function after() {
+        await closeMockAppWindow(this.currentTest.title);
+    });
+    basicRI2(fdc3, documentation_RI, ContextType.testContextZ);
+});
