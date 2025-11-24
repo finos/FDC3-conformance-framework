f7c415dda2be51d0105b71fb7f009965b26e2345
thorsent <terrence.thorsen@gmail.com>
2025-11-24 10:50:05 -0500

    Adjust WindowCloseWaitTime to 2 seconds

--- src/constants.ts
+++ src/constants.ts
@@ -7,7 +7,7 @@ const constants = {
   TestTimeout: 20000, // Tests that take longer than this (in milliseconds) will fail
   ManualTimeout: 100000, // Manual tests that take longer than this (in milliseconds) will fail
   WaitTime: 5000, // The amount of time to wait for mock apps to finish processing
-  WindowCloseWaitTime: 3000, // The amount of time to allow for clean-up of closed windows
+  WindowCloseWaitTime: 2000, // The amount of time to allow for clean-up of closed windows
   NoListenerTimeout: 120000, // the amount of time to allow for a DA to timeout waiting on a context or intent listener
   // FDC3 does not define this timeout so this should be extended if the DA uses a longer timeout
   ControlChannel: "app-control", //app channel used for passing messages between mock apps and tests
